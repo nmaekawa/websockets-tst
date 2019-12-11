65875e6c7072585895823b82fb389213af698123
nmaekawa <nmaekawa@g.harvard.edu>
2019-12-11 14:43:35 -0500

    moved data-massage scripts to gnuplot dir

--- target/ws-conn.sh
+++
@@ -1,9 +0,0 @@
-# samples on how to massage logs to make input for gnuplot
-
-cat /var/log/syslog | grep WSDISCONNECT | cut -d ' ' -f 4 | uniq -c
-
-cat /var/log/nginx/error.log | grep '\[error\]' | grep -v 'open\(\)' | grep premature | wc -lo
-
-cat /var/log/nginx/error.log | grep '\[error\]' | grep -v 'open\(\)' | grep 'reset by peer'  | wc -l
-
-ca error.log | grep 'reset by peer' | cut -d ' ' -f  2 | uniq -c > error-resetByPeer.dat
